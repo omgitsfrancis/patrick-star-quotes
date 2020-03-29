af863e78eb7cea8c103d934dba3b66298247e068
omgitsfrancis <omgitsfrancis@gmail.com>
2020-03-29 02:18:08 -0700

    removed sumbissions

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,9 @@
 
 import express from 'express';
 import quotes from './quotes';
-import submissions from './submissions';
 
 const router = express.Router();
 
 router.use('/quotes', quotes)
-router.use('/submissions', submissions)
 
 export default router;
